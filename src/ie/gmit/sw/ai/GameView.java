faa4691a43e8081ad87ea5262eb81d596e7a9b07
Alna132 <alannacurran@gmail.com>
2017-04-21 16:24:32 +0100

    Updated Code and functionality

--- src/ie/gmit/sw/ai/GameView.java
+++ src/ie/gmit/sw/ai/GameView.java
@@ -3,11 +3,10 @@ package ie.gmit.sw.ai;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class GameView extends JPanel implements ActionListener
-{
+public class GameView extends JPanel implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	public static final int DEFAULT_VIEW_SIZE = 800;
-	private int cellspan = 5;
+	private int cellspan = 5;	
 	private int cellpadding = 2;
 	private Maze maze;
 	private Sprite[] sprites;
@@ -17,119 +16,93 @@ public class GameView extends JPanel implements ActionListener
 	private int currentCol;
 	private boolean zoomOut = false;
 	private int imageIndex = -1;
-	private int offset = 48; //The number 0 is ASCII 48.
-	private Color[] reds = {new Color(255,160,122), new Color(139,0,0), new Color(255, 0, 0)}; //Animate enemy "dots" to make them easier to see
-
-	public GameView(Maze maze) throws Exception
-	{
+	private int offset = 48; 
+	private Color[] reds = {new Color(255,160,122), new Color(139,0,0), new Color(255, 0, 0)}; 
+	
+	public GameView(Maze maze) throws Exception{
 		this.maze = maze;
 		setBackground(Color.LIGHT_GRAY);
 		setDoubleBuffered(true);
 		timer = new Timer(300, this);
 		timer.start();
-	}//- End of GameView()
-
-	public void setCurrentRow(int row)
-	{
+	}//- End of GameView
+	
+	public void setCurrentRow(int row) {
 		if (row < cellpadding){
 			currentRow = cellpadding;
-		}else if (row > (maze.size() - 1) - cellpadding)
-		{
+		}else if (row > (maze.size() - 1) - cellpadding){
 			currentRow = (maze.size() - 1) - cellpadding;
-		}else
-		{
+		}else{
 			currentRow = row;
 		}//- End of if/else
-	}//- End of setCurrentRow()
+	}//- End of setCurrentRow
 
-	public void setCurrentCol(int col)
-	{
-		if (col < cellpadding)
-		{
+	public void setCurrentCol(int col) {
+		if (col < cellpadding){
 			currentCol = cellpadding;
-		}else if (col > (maze.size() - 1) - cellpadding)
-		{
+		}else if (col > (maze.size() - 1) - cellpadding){
 			currentCol = (maze.size() - 1) - cellpadding;
-		}
-		else
-		{
+		}else{
 			currentCol = col;
-		}//- End of if/else
-	}//- End of setCurrentCol()
+		}//- End of setCurrentCol
+	}
 
-	public void paintComponent(Graphics g)
-	{
+	public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
-
-        cellspan = zoomOut ? maze.size() : 5;
+              
+        cellspan = zoomOut ? maze.size() : 5;         
         final int size = DEFAULT_VIEW_SIZE/cellspan;
         g2.drawRect(0, 0, GameView.DEFAULT_VIEW_SIZE, GameView.DEFAULT_VIEW_SIZE);
-
-        for(int row = 0; row < cellspan; row++)
-        {
-        	for (int col = 0; col < cellspan; col++)
-        	{
+        
+        for(int row = 0; row < cellspan; row++) {
+        	for (int col = 0; col < cellspan; col++){  
         		int x1 = col * size;
         		int y1 = row * size;
-
-        		char ch = '0';
-
-        		if (zoomOut)
-        		{
-        			ch = maze.get(row, col);
-        			if (ch >= '5')
-        			{
-	        			if (row == currentRow && col == currentCol)
-	        			{
-	        				// changes the colour of the Spartan
-	        				g2.setColor(Color.ORANGE);
-	        			}
-	        			else
-	        			{
+        		
+        		//char ch = '0';
+        		int id = 0;
+       		
+        		if (zoomOut){
+        			id = maze.get(row, col).getId();
+        			if (id >= 5){
+	        			if (row == currentRow && col == currentCol){
+	        				g2.setColor(Color.YELLOW);
+	        			}else{
 	        				g2.setColor(reds[(int) (Math.random() * 3)]);
 	        			}//- End of inner if/else
         				g2.fillRect(x1, y1, size, size);
-        			}//- End of middle if
-        		}
-        		else
-        		{
-        			ch = maze.get(currentRow - cellpadding + row, currentCol - cellpadding + col);
-        		}//- End of outer if/else
+        			}//- End of if
+        		}else{
+        			id = maze.get(currentRow - cellpadding + row, currentCol - cellpadding + col).getId();
+        		}//- End of if/else
+        		
+        		imageIndex = id;
 
-        		imageIndex = (int) ch;
-        		imageIndex -= offset;
-        		if (imageIndex < 0)
-        		{
+        		if (imageIndex < 0){
         			g2.setColor(Color.LIGHT_GRAY);//Empty cell
-        			g2.fillRect(x1, y1, size, size);
-        		}else
-        		{
+        			g2.fillRect(x1, y1, size, size);   			
+        		}else{
         			g2.drawImage(sprites[imageIndex].getNext(), x1, y1, null);
         		}//- End of if/else
         	}//- End of inner for
         }//- End of outer for
-	}//- End of paintComponent()
+	}//- End of paintComponent
+	
+	public void toggleZoom(){
+		zoomOut = !zoomOut;		
+	}//- End of toggleZoom
 
-	public void toggleZoom()
-	{
-		zoomOut = !zoomOut;
-	}//- End of toggleZoom()
-
-	public void actionPerformed(ActionEvent e)
-	{
-		if (enemy_state < 0 || enemy_state == 5)
-		{
+	public void actionPerformed(ActionEvent e) {	
+		if (enemy_state < 0 || enemy_state == 5){
 			enemy_state = 6;
-		}else
-		{
+		}else{
 			enemy_state = 5;
 		}//- End of if/else
 		this.repaint();
-	}//- End of actionPerformed()
-
-	public void setSprites(Sprite[] sprites)
-	{
+	}//- End of actionPerformed
+	
+	public void setSprites(Sprite[] sprites){
 		this.sprites = sprites;
-	}//- End of setSprites()
+	}//- End of setSprites
 }//- End of GameView
